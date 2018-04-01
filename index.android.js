a25c225d69d3d28f5d3eb3cae0070dd22b6f14a5
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-04-01 20:04:42 -0300

    Add style with StylesSheets and add FlatList

--- index.android.js
+++ index.android.js
@@ -10,7 +10,7 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView,
+  FlatList,
   Image,
   Dimensions
 } from 'react-native';
@@ -34,30 +34,61 @@ export default class instaclone extends Component {
     }];
 
     return (
-      <ScrollView style={{ marginTop: 20 }} >
 
-        { fotos.map( foto => 
-        
-           <View key={ foto.id }>
-               <Text> { foto.usuario } </Text> 
-               <Image source = {
+
+      <FlatList
+        style={styles.container}
+        keyExtractor={ item => String(item.id) }
+        data={ fotos }
+        renderItem={ ({ item } ) =>
+           <View>
+              
+              <View style={styles.header}>
+
+                <Image source={
+                    require('./resources/imagem/foto.jpg')
+                  }
+                style={styles.profilePicture}
+                />
+
+                <Text> { item.usuario } </Text>
+
+              </View>
+              
+              <Image source={
                    require('./resources/imagem/foto.jpg')
                  }
-               style = {
-                 {
-                   width: width,
-                   height: width
-                 }
-               }
+               style={styles.postPicture}
                />
            </View>
-           
-         )}
 
 
-      </ScrollView>
+       }
+      /> 
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    marginTop: 10
+  },
+  header: {
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  profilePicture: {
+    marginRight: 10,
+    borderRadius: 20,
+    width: 40,
+    height: 40
+  },
+  postPicture: {
+    width: width,
+    height: width
+  }
+})
 
 AppRegistry.registerComponent('instaclone', () => instaclone);
